bbecb6ceddca3be0737798f7cc80874bc09ac57d
Precious <adeprecio3@gmail.com>
2024-05-08 12:15:46 +0100

    new change

---
+++ practice.py
@@ -0,0 +1,8 @@
+with open("leter.txt", "w") as file:
+    for i in range(50):
+        file.write("nothing is as hard as it seems, i just have to put my heart.\n")
+
+def count_alphabets(leter.txt):
+    alphabeth_count= {}
+with open("letter.txt", "correct.txt", "r") as file:
+          
